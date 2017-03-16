6f934c04c5e1940a9339dd4d7a7cee075ebccb3a
ravdeep003 <ravdeep003@gmail.com>
2017-03-15 18:49:37 -0700

    edit few changes

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    name = "John Stuart from the Daily Show"
-    # name = "CS190"
+    #name = "John Stuart"
+    name = "CS190"
     return render_template('hello.html', name=name)
     
 
